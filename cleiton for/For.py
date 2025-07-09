5b2192e56684b923a2bd261b0a5307c301bf535b
cleiton queiroz <145817122+Kileer87@users.noreply.github.com>
2025-07-08 21:53:23 -0300

    Add files via upload

---
+++ cleiton for/For.py
@@ -0,0 +1,55 @@
+
+
+# Inicializando os contadores
+# positivos = 0
+# negativos = 0
+
+# # Solicitando 10 números ao usuário
+# for i in range(10):
+#     numero = float(input(f"Digite o {i + 1}º número: "))
+#     if numero > 0:
+#         positivos += 1
+#     elif numero < 0:
+#         negativos += 1
+#     # Números iguais a zero não são contados neste caso
+
+# # Exibindo o resultado
+# print(f"\nQuantidade de números positivos: {positivos}")
+# print(f"Quantidade de números negativos: {negativos}")
+
+# Inicializando a soma
+# soma_pares = 0
+
+# Percorrendo números de 1 a 50
+# for numero in range(1, 51):
+#     if numero % 2 == 0:  # Verifica se é par
+#         soma_pares += numero
+
+# # Exibindo o resultado
+# print("A soma de todos os números pares de 1 a 50 é:", soma_pares)
+
+# # Solicita uma palavra ao usuário
+# palavra = input("Digite uma palavra: ")
+
+# # Inicializa o contador de vogais
+# quantidade_vogais = 0
+
+# # Percorre cada letra da palavra
+# for letra in palavra:
+#     if letra.lower() in 'aeiou':  # Verifica se a letra é vogal (considerando minúsculas)
+#         quantidade_vogais += 1
+
+# # Exibe o resultado
+# print("A palavra contém", quantidade_vogais, "vogais.")
+
+# soma = 0
+# for i in range(5):
+#     nota =float(input(f"digite {i+1}ª nota: "))
+#     soma += nota
+    
+# media = soma/ 5  
+# print("suma media é " ,media)
+# if media >= 6:
+#     print ("classificação: aprovado")
+# else:
+#     print ("clacificaçao: reprovado")
